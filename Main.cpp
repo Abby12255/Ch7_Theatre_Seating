dbff9f34f1970837499553afb594247103701220
Abby12255 <111085785+Abby12255@users.noreply.github.com>
2024-12-02 14:13:20 -0600

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <algorithm>
+
+using namespace std;
+
+void mason();
+void Abby();
+void Jullian();
+void seb();
